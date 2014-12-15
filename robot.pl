8d2c5924971bbbd3f3933508b6e4b5fc2a83f729
vladwelt <vladwelt@gmail.com>
2014-12-15 08:16:17 -0400

    [EDIT] Closed cycle and controlled overflow memory.

--- robot.pl
+++ robot.pl
@@ -25,57 +25,63 @@ dejar(paquete,robot,habitacion10) :-
     estar(robot,habitacion10).
 
 estar(robot,habitacion1) :- 
-    ir(robot,habitacion1).
+    ir(robot,habitacion1),!.
 estar(robot,habitacion2) :- 
-    ir(robot,habitacion2).
+    ir(robot,habitacion2),!.
 estar(robot,habitacion3) :- 
-    ir(robot,habitacion3).
+    ir(robot,habitacion3),!.
 estar(robot,habitacion4) :- 
-    ir(robot,habitacion4).
+    ir(robot,habitacion4),!.
 estar(robot,habitacion5) :- 
-    ir(robot,habitacion5).
+    ir(robot,habitacion5),!.
 estar(robot,habitacion6) :- 
-    ir(robot,habitacion6).
+    ir(robot,habitacion6),!.
 estar(robot,habitacion7) :- 
-    ir(robot,habitacion7).
+    ir(robot,habitacion7),!.
 estar(robot,habitacion8) :- 
-    ir(robot,habitacion8).
+    ir(robot,habitacion8),!.
 estar(robot,habitacion9) :- 
-    ir(robot,habitacion9).
+    ir(robot,habitacion9),!.
 estar(robot,habitacion10) :- 
-    ir(robot,habitacion10).
+    ir(robot,habitacion10),!.
 estar(robot,afuera) :- 
     ir(robot,afuera),!.
 estar(robot,almacen) :- 
-    ir(robot,almacen).
+    ir(robot,almacen),!.
 
 ir(robot, habitacion3) :- 
     estar(robot, habitacion2).
 ir(robot, habitacion2) :- 
-    estar(robot, habitacion1).
+    estar(robot, habitacion1);
+    estar(robot,habitacion3).
 ir(robot, habitacion1) :- 
-    estar(robot, afuera).
+    estar(robot, afuera);
+    estar(robot, habitacion2).
 
 ir(robot, habitacion7) :- 
     estar(robot, habitacion6).
 ir(robot, habitacion6) :- 
-    estar(robot, habitacion5).
+    estar(robot, habitacion5);
+    estar(robot, habitacion7).
 ir(robot, habitacion5) :- 
-    estar(robot, habitacion4).
-ir(robot, habitacion4) :- 
+    estar(robot, habitacion4);
     estar(robot, afuera).
+ir(robot, habitacion4) :- 
+    estar(robot, afuera);
+    estar(robot,habitacion5).
 
 ir(robot, habitacion10) :- 
     estar(robot, habitacion9).
 ir(robot, habitacion9) :- 
-    estar(robot, habitacion8).
+    estar(robot, habitacion8);
+    estar(robot, habitacion10).
 ir(robot, habitacion8) :- 
-    estar(robot, afuera).
+    estar(robot, afuera);
+    estar(robot, habitacion9).
 
 ir(robot,afuera) :- 
     estar(robot,almacen);
-    estar(robot,habitacion1);
     estar(robot,habitacion5);
-    estar(robot,habitacion8).
-
+    estar(robot,habitacion8);
+    estar(robot,habitacion1).
 
