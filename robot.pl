6150874bc5e265892c77d4e769e4b36e8d438874
vladwelt <vladwelt@gmail.com>
2014-12-15 07:46:44 -0400

    [EDIT] Dependencies defined for functors.

--- robot.pl
+++ robot.pl
@@ -1,37 +1,81 @@
-progenitor(clara, pedro).
-progenitor(pedro,jose).
-progenitor(ana,clara).
-progenitor(jose, clara).
-
-abuelo(X,Y) :- progenitor(X,Z) , progenitor(Z,Y).
-bisabuelo(X,Y) :- progenitor(X,Z) , abuelo(Z,Y).
-
-estar(robot,habitacion1).
-estar(robot,habitacion2).
-estar(robot,habitacion3).
-estar(robot,habitacion4).
-estar(robot,habitacion5).
-estar(robot,habitacion6).
-estar(robot,habitacion7).
-estar(robot,habitacion8).
-estar(robot,habitacion9).
-estar(robot,habitacion10).
-estar(robot,afuera).
 estar(robot,almacen).
 
-ir(robot,habitacion1).
-ir(robot,habitacion2).
-ir(robot,habitacion3).
-ir(robot,habitacion4).
-ir(robot,habitacion5).
-ir(robot,habitacion6).
-ir(robot,habitacion7).
-ir(robot,habitacion8).
-ir(robot,habitacion9).
-ir(robot,habitacion10).
-ir(robot,almacen).
-ir(robot,afuera).
+transportar(X,Y,Z) :- estar(Z,X),
+    dejar(paquete,Z,Y).
 
-recojer(paraguas,habitacion3).
-recojer(paraguas,habitacion8).
-recojer(paraguas,habitacion4).
+dejar(paquete,robot,habitacion1) :- 
+    estar(robot,habitacion1).
+dejar(paquete,robot,habitacion2) :- 
+    estar(robot,habitacion2).
+dejar(paquete,robot,habitacion3) :- 
+    estar(robot,habitacion3).
+dejar(paquete,robot,habitacion4) :- 
+    estar(robot,habitacion4).
+dejar(paquete,robot,habitacion5) :- 
+    estar(robot,habitacion5).
+dejar(paquete,robot,habitacion6) :- 
+    estar(robot,habitacion6).
+dejar(paquete,robot,habitacion7) :- 
+    estar(robot,habitacion7).
+dejar(paquete,robot,habitacion8) :- 
+    estar(robot,habitacion8).
+dejar(paquete,robot,habitacion9) :- 
+    estar(robot,habitacion9).
+dejar(paquete,robot,habitacion10) :- 
+    estar(robot,habitacion10).
+
+estar(robot,habitacion1) :- 
+    ir(robot,habitacion1).
+estar(robot,habitacion2) :- 
+    ir(robot,habitacion2).
+estar(robot,habitacion3) :- 
+    ir(robot,habitacion3).
+estar(robot,habitacion4) :- 
+    ir(robot,habitacion4).
+estar(robot,habitacion5) :- 
+    ir(robot,habitacion5).
+estar(robot,habitacion6) :- 
+    ir(robot,habitacion6).
+estar(robot,habitacion7) :- 
+    ir(robot,habitacion7).
+estar(robot,habitacion8) :- 
+    ir(robot,habitacion8).
+estar(robot,habitacion9) :- 
+    ir(robot,habitacion9).
+estar(robot,habitacion10) :- 
+    ir(robot,habitacion10).
+estar(robot,afuera) :- 
+    ir(robot,afuera),!.
+estar(robot,almacen) :- 
+    ir(robot,almacen).
+
+ir(robot, habitacion3) :- 
+    estar(robot, habitacion2).
+ir(robot, habitacion2) :- 
+    estar(robot, habitacion1).
+ir(robot, habitacion1) :- 
+    estar(robot, afuera).
+
+ir(robot, habitacion7) :- 
+    estar(robot, habitacion6).
+ir(robot, habitacion6) :- 
+    estar(robot, habitacion5).
+ir(robot, habitacion5) :- 
+    estar(robot, habitacion4).
+ir(robot, habitacion4) :- 
+    estar(robot, afuera).
+
+ir(robot, habitacion10) :- 
+    estar(robot, habitacion9).
+ir(robot, habitacion9) :- 
+    estar(robot, habitacion8).
+ir(robot, habitacion8) :- 
+    estar(robot, afuera).
+
+ir(robot,afuera) :- 
+    estar(robot,almacen);
+    estar(robot,habitacion1);
+    estar(robot,habitacion5);
+    estar(robot,habitacion8).
+
+
